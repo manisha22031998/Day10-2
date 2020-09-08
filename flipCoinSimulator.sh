6a2f6bb3ca03d7ab335dd8b9a3fba29baad79c21
unknown <manishasingh2203@gmail.com>
2020-09-08 23:29:29 +0530

    UC2 Done

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,11 +1,18 @@
 #! /bin/bash
 echo THIS DISPLAYS THE WINNER HEADS OR TAILS
-heads=1
-tails=2
-flip=$((RANDOM%2))
-if [ $flip -eq 1 ]
-then
-	echo H
-else
-	echo T
-fi
+#H=1,T=0
+read -p "ENTER THE NUMBER OF TIMES YOU WANT TO FLIP" flipcoin
+heads=0
+tails=0
+for (( i=1;i<=$flipcoin;i++ ))
+do
+	flip=$((RANDOM%2))
+	if [ $flip -eq 1 ]
+	then
+		((heads++))
+	else
+		((tails++))
+	fi
+done
+echo H $heads
+echo T $tails
